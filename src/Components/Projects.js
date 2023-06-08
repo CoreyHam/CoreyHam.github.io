86e7abe4860cf567962ac84c886b43c60bcfdc5b
Corey <corey.hampton@student.awesomeinc.org>
2023-06-08 16:32:58 +0000

    upload projects with demos

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import saturnUniversity from './Images/saturn-university-site.png'
 import tictactoe from './Images/tic-tac-toe.png'
+import todo from './Images/to-do.png'
 
 export default function About({ fixed }) {
     return (
@@ -42,12 +43,48 @@ export default function About({ fixed }) {
                                     Saturn University
                                 </h4>
                                 <p className="text-md font-light mt-2 text-gray-600">
-                                    The Saturn University website features a user-friendly design that captures the essence of a fictional academic institution nestled among Saturn's rings.
+                                    The Saturn University website features a user-friendly design that captures the essence of a fictional academic institution nestled among Saturn's rings. This project is still being worked on.
                                 </p>
                             </blockquote>
                             <div className="flex justify-evenly py-2 text-lg font-bold" >
-                                <a className="p-4 hover:text-red-700" href="/">Live Demo</a>
+                                <a className="p-4 hover:text-red-700" href="https://coreyham.github.io/university/">Live Demo</a>
                                 <a className="p-4 hover:text-red-700" href="https://github.com/CoreyHam/university">GitHub <i class="fa-brands fa-github" ></i> </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-6/12 px-4">
+                        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                            <img
+                                alt="..."
+                                src={todo}
+                                className="w-full align-middle rounded-t-lg"
+                            />
+                            <blockquote className="relative p-8 mb-4">
+                                <svg
+                                    preserveAspectRatio="none"
+                                    viewBox="0 0 583 95"
+                                    className="absolute left-0 w-full block"
+                                    style={{
+                                        height: "95px",
+                                        top: "-94px"
+                                    }}
+                                >
+                                    <polygon
+                                        points="-30,95 583,95 583,65"
+                                        className="text-white fill-current"
+                                    ></polygon>
+                                </svg>
+                                <h4 className="text-xl font-bold text-gray-600">
+                                    Todos
+                                </h4>
+                                <p className="text-md font-light mt-2 text-gray-600">
+                                    The to-do project was a coding challenge where I created a fully interactive to-do list while learning React. This project took me one week.
+                                </p>
+                            </blockquote>
+                            <div className="flex justify-evenly py-2 text-lg font-bold" >
+                                <a className="p-4 hover:text-red-700" href="https://coreyham.github.io/to-do/">Live Demo</a>
+                                <a className="p-4 hover:text-red-700" href="https://github.com/CoreyHam/to-do">GitHub <i class="fa-brands fa-github" ></i> </a>
 
                             </div>
                         </div>
